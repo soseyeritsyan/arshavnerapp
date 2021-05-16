b0eb9199721ceb00782deddbf7e49f32191dd36a
Sose <sose@robomart.co>
2021-05-16 15:36:02 +0400

    first commit

---
+++ ArshavnerApp/ArshavnerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArshavnerApp
+//
+//  Created by sose yeritsyan on 5/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
