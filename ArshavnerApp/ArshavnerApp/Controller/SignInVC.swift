32f0e33602fb4bbaf4f543d04fbef747f212867f
ShaqueYeritsyan <yeritsyanshake@gmail.com>
2021-05-24 20:27:45 +0400

    made some changes

--- ArshavnerApp/ArshavnerApp/Controller/SignInVC.swift
+++ ArshavnerApp/ArshavnerApp/Controller/SignInVC.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
             
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func clickedSignIn(_sender: UIButton) {
+        
+    }
 
+    @IBAction func clickedDontHaveAccount(_sender: UIButton) {
+        
+    }
 }
 
